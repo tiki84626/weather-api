6c1e5973592e83789a448a89670cc2c1b5818b34
Andrew Malkin <andymalkin@gmail.com>
2016-10-12 07:03:47 -0700

    create template file structure for bower, gulp, JS project

---
+++ js/template-interface.js
@@ -0,0 +1,13 @@
+var template = require('./../js/template.js').TemplateModule;
+
+//<!-- Back End -->
+console.log('basic javascript is functioning')
+
+//<!-- Front End  -->
+$(document).ready(function(){
+if (jQuery) {
+console.log('jQuery 3.1.0 is loaded');
+} else {
+console.log('jQuery is not loaded');
+    }
+});
---
+++ js/template.js
@@ -0,0 +1,1 @@
+exports.TemplateModule = Template;
